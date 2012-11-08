855a8b2c64b81f44c8c84d80d41d1c6373abf0fb
Naomi Dushay <ndushay@stanford.edu>
2012-11-07 16:54:02 -0800

    use @mods_rec instead of @mods in spec

--- spec/top_level_elmnts_simple_spec.rb
+++ spec/top_level_elmnts_simple_spec.rb
@@ -3,28 +3,26 @@ require 'spec_helper'
 describe "Mods Top Level Elements that do not have Child Elements" do
 
   before(:all) do
-    @mods = Mods::Record.new
+    @mods_rec = Mods::Record.new
   end
 
   it "should get the text contents of any single simple (cannot have child elements) top level element" do
     Mods::TOP_LEVEL_ELEMENTS_SIMPLE.each { |elname|
-      @mods.from_str("<mods><#{elname}>hi</#{elname}></mods>")
-      @mods.send(elname.to_sym).should == ["hi"]
+      @mods_rec.from_str("<mods><#{elname}>hi</#{elname}></mods>")
+      @mods_rec.send(elname.to_sym).should == ["hi"]
     }
   end
   
   it "should get the text contents of any single complex top level element instance with no child elements" do
     pending "to be implemented"
     Mods::TOP_LEVEL_ELEMENTS_COMPLEX.each { |elname|
-      @mods.from_str("<mods><#{elname}>hi</#{elname}></mods>")
-      @mods.send(elname.to_sym).should == ["hi"]
+      @mods_rec.from_str("<mods><#{elname}>hi</#{elname}></mods>")
+      @mods_rec.send(elname.to_sym).should == ["hi"]
     }
   end
   
   it "should return an array of strings when there are multiple occurrences of simple top level elements" do
-    str =  '<mods><note>hi</note><note>hello</note></mods>'
-    @mods.from_str(str)
-    @mods.note.should == ["hi", "hello"]
+    @mods_rec.from_str('<mods><note>hi</note><note>hello</note></mods>').note.should == ["hi", "hello"]
   end
   
   it "should deal with camelcase vs. ruby underscore convention" do
