6f9300b957b493ce96bf3d14ac9c24fdb5e93319
Chris Beer <cabeer@stanford.edu>
2015-01-23 13:11:15 -0800

    Load gem dependencies before loading the rest of the mods gem

--- lib/mods.rb
+++ lib/mods.rb
@@ -1,18 +1,16 @@
-require 'mods/constants'
-require 'mods/nom_terminology'
-require 'mods/marc_country_codes'
-require 'mods/marc_geo_area_codes'
-require 'mods/marc_relator_codes'
-require 'mods/name'
-require 'mods/reader'
-require 'mods/record'
-require 'mods/subject'
-require 'mods/title_info'
-require 'mods/version'
-
 require 'nokogiri'
 require 'nom/xml'
 
 module Mods
-    
+  require 'mods/constants'
+  require 'mods/nom_terminology'
+  require 'mods/marc_country_codes'
+  require 'mods/marc_geo_area_codes'
+  require 'mods/marc_relator_codes'
+  require 'mods/name'
+  require 'mods/reader'
+  require 'mods/record'
+  require 'mods/subject'
+  require 'mods/title_info'
+  require 'mods/version'
 end
